428eb8642c1c385396893e5234b22d86c684a89e
Mareedu Venkata Pavan <84392859+venkatapavan99@users.noreply.github.com>
2025-08-17 15:22:57 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,25 +1,23 @@
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
     @Test
-    void testGoogleHomePage() {
-        // Setup ChromeDriver using WebDriverManager
+    public void openGoogle() {
+        // Setup Chrome driver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        try {
-            driver.get("https://www.google.com");
-            String title = driver.getTitle();
-            assertTrue(title.contains("Google"), "Title does not contain Google!");
-        } finally {
-            driver.quit();
-        }
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+
+        Assert.assertTrue(title.contains("Google"));
+
+        driver.quit();
     }
 }
-
